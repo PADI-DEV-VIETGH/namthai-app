190674e5985fbb9ea61346648a6d39b034fb977c
PADI-DEV-VIETGH <vietgh@paditech.com>
2022-03-07 15:20:14 +0700

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,58 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+use Illuminate\Support\Facades\Http;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    private $baseUrl;
+
+    public function setOptions($options = [])
+    {
+        if(isset($options['base_url']) && $options['base_url']){
+            $this->baseUrl = $options['base_url'];
+        }
+    }
+
+    public function get($endPoint = "", $params = [])
+    {
+        try{
+            $url = $this->baseUrl.'/'.$endPoint;
+            $response = Http::get($url, $params);
+            if($response->successful()){
+                $data = [];
+                if($response->successful()){
+                    $data = $response->json();
+                }
+                return $data;
+            }else{
+                throw new \Exception($response->getReasonPhrase());
+            }
+        }catch (\Exception $e){
+            throw $e;
+        }
+
+    }
+
+    public function post($endPoint = "", $header =[], $params = [])
+    {
+        try{
+            $url = $this->baseUrl.'/'.$endPoint;
+            $response = Http::withHeaders($header)
+                ->post($url, $params);
+            if($response->successful()){
+                $data = [];
+                if($response->successful()){
+                    $data = $response->json();
+                }
+                return $data;
+            }else{
+                throw new \Exception($response->getReasonPhrase());
+            }
+        }catch (\Exception $e){
+            throw $e;
+        }
+
+    }
 }
--- resources/views/app_farm/login.blade.php
+++ resources/views/app_farm/login.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>bike code list</title>
+    <title>Farm login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -28,46 +28,48 @@
 <body class="page-header-fixed page-quick-sidebar-over-content">
 
     <div class="page-container">
-
-        <div class="page-content-wrapper page-register">
-            <div class="page-content">
-                <div class="main-body-content member-page">
-                    <div class="member-title-table">
-                        <div class="logo text-center"><img src="/namthai/assets/images/logo.png" /></div>
-                        <div class="title-page full border-bottom mrb-30">
-                            <h3 class="bold text-center">Đăng nhập</h3>
+        <form action="{{ route('app_farm.login') }}" method="post">
+            @csrf
+            <div class="page-content-wrapper page-register">
+                <div class="page-content">
+                    <div class="main-body-content member-page">
+                        <div class="member-title-table">
+                            <div class="logo text-center"><img src="/namthai/assets/images/logo.png" /></div>
+                            <div class="title-page full border-bottom mrb-30">
+                                <h3 class="bold text-center">Đăng nhập</h3>
+                            </div>
+                            <div class="full border-full pad-20-30 mrb-40">
+                                <div class="full mrb-10">
+                                    <div class="col-md-12 line-30">
+                                        <label class="control-label">Số điện thoại</label>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <input class="form-control" type="text" placeholder="0123456789" name="phone_number"/>
+                                    </div>
+                                </div>
+                                <div class="full mrb-10">
+                                    <div class="col-md-12 line-30">
+                                        <label class="control-label">Mật khẩu</label>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <input class="form-control" type="password" placeholder="0123456789" name="password"/>
+                                    </div>
+                                    <div class="col-md-12 text-right"><a class="text-link-otp" href="#">Quên mật khẩu ?</a>
+                                    </div>
+                                </div>
+                                <div class="full text-center">
+                                    <button class="btn green pad-8-50">Đăng nhập</button>
+                                </div>
+                            </div>
                         </div>
-                        <div class="full border-full pad-20-30 mrb-40">
-                            <div class="full mrb-10">
-                                <div class="col-md-12 line-30">
-                                    <label class="control-label">Số điện thoại</label>
-                                </div>
-                                <div class="col-md-12">
-                                    <input class="form-control" type="text" placeholder="0123456789" />
-                                </div>
-                            </div>
-                            <div class="full mrb-10">
-                                <div class="col-md-12 line-30">
-                                    <label class="control-label">Mật khẩu</label>
-                                </div>
-                                <div class="col-md-12">
-                                    <input class="form-control" type="password" placeholder="0123456789" />
-                                </div>
-                                <div class="col-md-12 text-right"><a class="text-link-otp" href="#">Quên mật khẩu ?</a>
-                                </div>
-                            </div>
-                            <div class="full text-center">
-                                <button class="btn green pad-8-50">Đăng nhập</button>
-                            </div>
-                        </div>
+                        <!--.table-member-mng-->
+                        <!-- .block-1-->
                     </div>
-                    <!--.table-member-mng-->
-                    <!-- .block-1-->
+                    <!-- .member-main-->
                 </div>
-                <!-- .member-main-->
+                <!-- .page-content-->
             </div>
-            <!-- .page-content-->
-        </div>
+        </form>
         <!-- .member-->
     </div>
     <!--.page-container-->
