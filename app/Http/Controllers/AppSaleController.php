243da769b12c411f2f09f4bb0dcf69217ec0007f
VuongBV <vuongbv@paditech.com>
2022-03-07 15:20:44 +0700

    update

--- app/Http/Controllers/AppSaleController.php
+++ app/Http/Controllers/AppSaleController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AppSaleController extends Controller
 {
     public function login()
     {
         return view('app_sale.login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $credentials = $request->only(['phone_number', 'password']);
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('app_sale.home_not_checkin');
+        }
+
+        return view('app_sale.home_not_checkin');
     }
 }
--- resources/views/app_sale/login.blade.php
+++ resources/views/app_sale/login.blade.php
@@ -36,31 +36,34 @@
                         <div class="title-page full border-bottom mrb-30">
                             <h3 class="bold text-center">Đăng nhập</h3>
                         </div>
-                        <div class="full border-full pad-20-30 mrb-40">
-                            <div class="full mrb-10">
-                                <div class="col-md-12 line-30">
-                                    <label class="control-label">Số điện thoại</label>
+                        <form method="post" action="{{ route('app_sale.login') }}">
+                            @csrf
+                            <div class="full border-full pad-20-30 mrb-40">
+                                <div class="full mrb-10">
+                                    <div class="col-md-12 line-30">
+                                        <label class="control-label">Số điện thoại</label>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <input class="form-control" name="phone_number" type="text" placeholder="0123456789" />
+                                    </div>
                                 </div>
-                                <div class="col-md-12">
-                                    <input class="form-control" type="text" placeholder="0123456789" />
+                                <div class="full mrb-10">
+                                    <div class="col-md-12 line-30">
+                                        <label class="control-label">Mật khẩu</label>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <input class="form-control" name="password" type="password" placeholder="0123456789" />
+                                    </div>
+                                </div>
+                                <div class="full text-center">
+                                    <div class="col-md-12">
+                                        <button type="submit" class="btn green pad-8-50 wdt-100">Đăng nhập</button>
+                                    </div>
+                                    <div class="col-md-12 text-left"><a class="text-link-otp" href="#">Quên mật khẩu ?</a>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="full mrb-10">
-                                <div class="col-md-12 line-30">
-                                    <label class="control-label">Mật khẩu</label>
-                                </div>
-                                <div class="col-md-12">
-                                    <input class="form-control" type="password" placeholder="0123456789" />
-                                </div>
-                            </div>
-                            <div class="full text-center">
-                                <div class="col-md-12">
-                                    <button class="btn green pad-8-50 wdt-100">Đăng nhập</button>
-                                </div>
-                                <div class="col-md-12 text-left"><a class="text-link-otp" href="#">Quên mật khẩu ?</a>
-                                </div>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                     <!--.table-member-mng-->
                     <!-- .block-1-->
